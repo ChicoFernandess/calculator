c68aa24ef0c647305bead91c99c0a73f709d522a
ChicoFernandess <franciscofeernandes96@hotmail.com>
2025-01-14 14:14:30 +0000

    change layout, add reset button and negative number button

--- index.js
+++ index.js
@@ -14,3 +14,35 @@ function getSubmit(){
 
     elem.value = eval(expression);
 }
+
+function resetCalculator(){
+    let elem = document.getElementById("calculatorinput");
+    elem.value = '';
+}
+
+function turnNegPos(){
+    let elem = document.getElementById("calculatorinput");
+    let currentValue = elem.value;
+
+    let lastOperatorIndex = Math.max(
+        currentValue.lastIndexOf("+"),
+        currentValue.lastIndexOf("-"),
+        currentValue.lastIndexOf("*"),
+        currentValue.lastIndexOf("/")
+    );
+
+    // Extract the part of the string after the last operator
+    let beforeLastNumber = currentValue.slice(0, lastOperatorIndex + 1); // Part before the last number
+    let lastNumber = currentValue.slice(lastOperatorIndex + 1); // Part after the last operator
+
+    if (lastNumber.startsWith("-")) {
+        // If the last number is negative, make it positive
+        lastNumber = lastNumber.substring(1);
+    } else {
+        // If the last number is positive, make it negative
+        lastNumber = "-" + lastNumber;
+    }
+
+    // Rebuild the input value with the toggled last number
+    elem.value = beforeLastNumber + lastNumber;
+}
